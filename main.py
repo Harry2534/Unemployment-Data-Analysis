817578edde5f0aa15abfd37d86a215e182facaf0
Harry2534 <hparappukkaran@gmail.com>
2025-01-14 13:37:41 +1000

    ARIMA forecast of unemployment in AU

--- main.py
+++ main.py
@@ -3,166 +3,189 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import streamlit as st
+import altair as alt
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.tsaplots import plot_predict
+import statsmodels.api as sm
+from streamlit_extras.stylable_container import stylable_container
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Exploratory Data Analysis",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
-st.title("The Impact of Unemployment on Australia's Economy")
+alt.themes.enable("dark")
 
+db = pd.read_excel("6202006.xlsx", 'Data1', header=9)
+db = db.dropna(how='all')
 
-col1, col2, col3 = st.columns([1, 5, 1])
+try:
+    db['Date'] = pd.to_datetime(db.iloc[:, 0], format='%b-%Y', errors='coerce')
+except Exception as e:
+    print(f"Error converting dates: {e}")
 
-with col1:
-    pass
+db.dropna(subset=['Date'], inplace=True)
 
-with col2:
-    st.write("""
-    <div style="font-size: 26px;"> 
-        Unemployment serves as a vital barometer of a nation's economic health and the stability of its workforce. For policymakers, economists, and stakeholders, it provides essential insights into labour market conditions, shaping decisions on fiscal strategies and monetary policies.
-        This Analysis takes a deep dive into Queensland's Labour Force data, exploring historical trends and unveiling a predictive model for forecasting future unemployment rates. By analyzing the evolution of unemployment over time, we aim to shed light on its potential trajectory in the coming months.
-        Our findings will equip government officials, economists, and other stakeholders with valuable insights to address labour market challenges and make informed decisions. Stay with us as we uncover the story behind the numbers and chart a path for Queensland's economic future.
-    </div>
-    """, unsafe_allow_html=True)
+db.set_index('Date', inplace=True)
 
-with col3:
-    pass
+if 'A84423620T' in db.columns:
+    db.rename(columns={'A84423620T': 'Unemployment Rate'}, inplace=True)
+    db = db[['Unemployment Rate']]
+else:
+    print(f"Series ID {'A2325846C'} not found.")
 
-
-
-def load_and_process_data(file_name, sheet_name, date_col_idx, series_id, new_col_name):
-    db = pd.read_excel(file_name, sheet_name=sheet_name, header=9)
-    db = db.dropna(how='all')
-
-    try:
-        db['Date'] = pd.to_datetime(db.iloc[:, date_col_idx], format='%b-%Y', errors='coerce')
-    except Exception as e:
-        print(f"Error converting dates: {e}")
+Unemployment_Rate = db.loc["2011-01-01":"2023-12-01"]
 
 
-    db.dropna(subset=['Date'], inplace=True)
 
-    db.set_index('Date', inplace=True)
+col = st.columns((1.5, 4.5, 2.5), gap='medium')
 
-    if series_id in db.columns:
-        db.rename(columns={series_id: new_col_name}, inplace=True)
-        db = db[[new_col_name]]
+
+with col[0]:
+    highest_value = Unemployment_Rate['Unemployment Rate'].max()
+    highest_date = Unemployment_Rate['Unemployment Rate'].idxmax()
+
+    lowest_value = Unemployment_Rate['Unemployment Rate'].min()
+    lowest_date = Unemployment_Rate['Unemployment Rate'].idxmin()
+
+    Unemployment_Rate = Unemployment_Rate.sort_index()
+    st.subheader('Unemployment Rate Insights')
+    st.markdown(f"**🔺 Highest Unemployment Rate:** {highest_value:.2f}% on {highest_date.strftime('%B %d, %Y')}")
+    st.markdown(f"**🔻 Lowest Unemployment Rate:** {lowest_value:.2f}% on {lowest_date.strftime('%B %d, %Y')}")
+
+    latest_date = "2023-12-01"
+    earlier_date = "2018-12-01"
+
+    latest_unemployment_rate = Unemployment_Rate.loc[latest_date, 'Unemployment Rate']
+    earlier_unemployment_rate = Unemployment_Rate.loc[earlier_date, 'Unemployment Rate']
+    unemployment_change = latest_unemployment_rate - earlier_unemployment_rate
+
+    st.markdown(f"**📅 Unemployment Rate on {earlier_date}:** {earlier_unemployment_rate:.2f}%")
+    st.markdown(f"**📅 Unemployment Rate on {latest_date}:** {latest_unemployment_rate:.2f}%")
+
+    if unemployment_change > 0:
+        st.markdown(f"**📊 Change in Unemployment Rate (2018-2023):** +{unemployment_change:.2f}%")
     else:
-        print(f"Series ID {series_id} not found.")
+        st.markdown(f"**📊 Change in Unemployment Rate (2018-2023):** {unemployment_change:.2f}%")
 
+with col[1]:
+    unemployment_filtered = Unemployment_Rate.loc['2012-01-01':'2024-12-31']
 
-    return db.loc['2011-01-01':'2023-12-01'].resample('QE').mean()
+    fig, ax = plt.subplots(figsize=(11, 5))
+    ax.plot(unemployment_filtered.index, unemployment_filtered['Unemployment Rate'], color='tab:red')
+    ax.set_title('Unemployment Rate (2012-2024)', fontsize=20)
+    ax.set_ylabel('UR Value', fontsize=16)
+    ax.set_xlabel('Date', fontsize=16)
+    ax.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
 
-
-Consumer_Price_Index = load_and_process_data(
-    '640101.xlsx',
-    sheet_name='Data1',
-    date_col_idx=0,
-    series_id='A2325846C',
-    new_col_name='Consumer Price Index'
-)
-
-Unemployment_Rate  = load_and_process_data(
-    '6202006.xlsx',
-    sheet_name='Data1',
-    date_col_idx=0,
-    series_id='A84423620T',
-    new_col_name='Unemployment Rate'
-)
-
-
-Cash_Rate_Target  = load_and_process_data(
-    'f01d.xlsx',
-    sheet_name='Data',
-    date_col_idx=0,
-    series_id='04-Oct-2024',
-    new_col_name='Cash Rate Target'
-)
-
-combined_quarterly = pd.concat([Unemployment_Rate, Cash_Rate_Target, Consumer_Price_Index], axis=1)
-
-fig, axes = plt.subplots(3, 1, figsize=(9, 7), sharex=True)
-
-titles = ['Unemployment Quarterly', 'Cash Rate Quarterly', 'Inflation Quarterly']
-
-for i, column in enumerate(combined_quarterly.columns):
-    rolling_mean = combined_quarterly[column].rolling(window=4).mean()
-    rolling_std = combined_quarterly[column].rolling(window=4).std()
-
-    axes[i].plot(combined_quarterly.index, combined_quarterly[column], label=column, marker='o', alpha=0.5)
-    axes[i].fill_between(combined_quarterly.index, rolling_mean - rolling_std, rolling_mean + rolling_std, alpha=0.2)
-
-    axes[i].set_title(titles[i])
-    axes[i].set_ylabel('Values')
-    axes[i].legend()
-    axes[i].grid()
-
-plt.xlabel('Date')
-plt.tight_layout()
-
-col1, col2 = st.columns([1, 1])
-
-with col1:
+    st.pyplot(fig)
     st.write("")
     st.write("")
+
+    train_data = Unemployment_Rate.loc['2020':'2024'].copy()
+    train_data['MA-4'] = train_data['Unemployment Rate'].rolling(12).mean()
+    train_data['Unemployment detrended'] = train_data['Unemployment Rate'] - train_data['MA-4']
+    raw_monthly_means = train_data.groupby(train_data.index.month)['Unemployment detrended'].mean()
+    adjustment = raw_monthly_means.sum() / 12.0
+    monthly_means = raw_monthly_means - adjustment
+    seasonal = np.tile(monthly_means, int(np.floor(len(train_data) / 12)))
+    train_data['Unemployment seasonal'] = seasonal
+    train_data['Unemployment residual'] = train_data['Unemployment detrended'] - train_data['Unemployment seasonal']
+    trend_d1 = train_data['MA-4'].diff()
+    trend_d2 = trend_d1.diff()
+    train_data.index = pd.to_datetime(train_data.index)
+    train_data = train_data.asfreq('MS')
+    endog = train_data["MA-4"]
+    arima_2_2_1 = ARIMA(endog, order=(2, 2, 1)).fit()
+    # print(arima_2_2_1.summary())
+    # fig = plt.figure(figsize=(16, 9))
+    # fig = arima_2_2_1.plot_diagnostics(fig=fig, lags=24)
+    arima_fcst = arima_2_2_1.get_forecast(steps=24)
+    arima_predictions = pd.DataFrame(arima_fcst.predicted_mean)
+    arima_predictions.rename(columns={"predicted_mean": "trend"}, inplace=True)
+    fig, ax_arima_fcst = plt.subplots(figsize=(24, 6))
+    train_data['MA-4'].plot(label='Original', ax=ax_arima_fcst)
+    seasonal_values = monthly_means.tolist() * (len(arima_predictions) // len(monthly_means))
+    arima_predictions.loc[:, 'seasonal'] = seasonal_values[:len(arima_predictions)]
+    arima_predictions = arima_predictions.copy()
+    arima_predictions.loc[:, 'trend+seasonal'] = arima_predictions['trend'] + arima_predictions['seasonal']
+    fig, ax_arima_fcst = plt.subplots(figsize=(24, 6))
+    train_data['MA-4'].plot(label='Original', ax=ax_arima_fcst)
+    arima_predictions['trend'].plot(label="ARIMA(2,2,1) trend fcst", ax=ax_arima_fcst)
+    arima_predictions['trend+seasonal'].plot(label="Trend+seasonal fcst", ax=ax_arima_fcst)
+    fig, ax_arima_fcst = plt.subplots(figsize=(24, 6))
+    train_data['MA-4'].plot(label='Original', ax=ax_arima_fcst)
+    arima_predictions['trend'].plot(label="ARIMA(2,2,1) trend fcst", ax=ax_arima_fcst)
+    arima_predictions['trend+seasonal'].plot(label="Trend+seasonal fcst", ax=ax_arima_fcst)
+    arima_predictions = pd.concat([arima_predictions, arima_fcst.conf_int()], axis=1)
+    arima_predictions.rename(columns={"lower MA-4": "trend lower CI", "upper MA-4": "trend upper CI"}, inplace=True)
+    arima_predictions["seasonal lower CI"] = arima_predictions["trend lower CI"] + arima_predictions['seasonal']
+    arima_predictions["seasonal upper CI"] = arima_predictions["trend upper CI"] + arima_predictions['seasonal']
+    fig, ax_arima_fcst = plt.subplots(figsize=(24, 8), dpi=100)
+    train_data['Unemployment Rate'].plot(label='Actual Unemployment Rate', ax=ax_arima_fcst, color='blue', linewidth=2)
+    arima_predictions['trend+seasonal'].plot(ax=ax_arima_fcst, color='orange', label='Predicted', linewidth=2)
+    arima_predictions['seasonal upper CI'].plot(ax=ax_arima_fcst, color='grey', linestyle='--',
+                                                label='Upper Confidence Interval', linewidth=1)
+    arima_predictions['seasonal lower CI'].plot(ax=ax_arima_fcst, color='grey', linestyle='--',
+                                                label='Lower Confidence Interval', linewidth=1)
+
+    ax_arima_fcst.fill_between(
+        arima_predictions.index,
+        arima_predictions['seasonal lower CI'],
+        arima_predictions['seasonal upper CI'],
+        color='grey',
+        alpha=0.2,
+        label='Confidence Interval'
+    )
+
+    ax_arima_fcst.set_title('ARIMA Forecast: Unemployment Rate with Confidence Intervals', fontsize=40, pad=20)
+    ax_arima_fcst.set_xlabel('Date', fontsize=40)
+    ax_arima_fcst.set_ylabel('Unemployment Rate', fontsize=40)
+    ax_arima_fcst.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.7)
+    ax_arima_fcst.legend(loc='lower left', fontsize=12, frameon=True, shadow=True, borderpad=1)
+    plt.tight_layout()
     st.pyplot(fig)
 
-with col2:
-    st.markdown("""
-        <div style="font-size: 24px;">
-            <br><br>                                                                                                                                                                                   
-            Unemployment Quarterly: Unemployment fluctuated between approximately 5% and 7% from 2011 to around 2020. After 2020, there was a noticeable spike, with unemployment rising to around 
-            7.5%, likely due to the COVID-19 pandemic's economic impact. However, from mid-2021 onwards, there is a sharp decline, bringing unemployment down to around 4%. Rolling Mean and 
-            Standard Deviation: The shaded blue area shows the standard deviation, indicating increased volatility around the 2020 spike, followed by stabilization as the unemployment rate 
-            decreases post-2021. 
-        </div>
-    """, unsafe_allow_html=True)
-    st.write("")
-    st.write("")
-    st.write("")
 
-    st.markdown("""
-            <div style="font-size: 24px;">
-            <br><br>  
-                Cash Rate Quarterly: There is a steady decline in the cash rate from around 4.75% in 2011 to nearly 0% by 2020, reflecting the global trend of low interest rates 
-                during this period. In 2021-2022, there's a significant shift as rates begin to climb rapidly, reaching around 4% by 2023, in response to inflationary pressures. Rolling Mean and 
-                Standard Deviation: The cash rate's decline is very stable from 2011 to 2020 (narrow shaded area), but volatility increases significantly during the recent hike, reflecting uncertainty
-                in financial markets as central banks respond to inflation. 
 
-            </div>
-        """, unsafe_allow_html=True)
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
+with col[2]:
+    st.subheader('Decomposition of Unemployment Rate: Trend, Seasonal, and Residual Components')
 
-    st.markdown("""
-            <div style="font-size: 24px;">  
-            <br><br> 
-                Inflation Quarterly: Inflation, represented by the Consumer Price Index (CPI), shows a steady upward trend, with relatively
-                moderate growth between 2011 and 2020. However, after 2020, there is a steep rise in inflation, with the CPI increasing significantly, reflecting the post-pandemic inflationary period. 
-                Rolling Mean and Standard Deviation: The shaded region indicates relatively low volatility in the earlier years, but from 2020 onwards, there’s a notable increase in both the CPI and 
-                its standard deviation, illustrating the sharp rise in inflation. Overall Insights: The unemployment rate and inflation were relatively stable, and the cash rate was on a steady 
-                decline pre 2020. Economic disruption is evident, likely triggered by the COVID-19 pandemic post 2020. There’s a spike in unemployment, followed by a recovery, a sharp increase in 
-                inflation, and a rise in cash rates to counter inflation.
-            </div>
-        """, unsafe_allow_html=True)
+    Unemployment_Rate['MA-4'] = Unemployment_Rate['Unemployment Rate'].rolling(12).mean()
+    Unemployment_Rate['Unemployment-detrended'] = Unemployment_Rate['Unemployment Rate'] - Unemployment_Rate['MA-4']
+    raw_monthly_means = Unemployment_Rate.groupby(Unemployment_Rate.index.month)['Unemployment-detrended'].mean()
+    adjustment = raw_monthly_means.sum() / 12.0
+    monthly_means = raw_monthly_means - adjustment
+    seasonal = np.tile(monthly_means, int(np.floor(len(Unemployment_Rate) / 12)))
+    Unemployment_Rate['Unemployment-seasonal'] = seasonal
+    Unemployment_Rate['Unemployment_residual'] = Unemployment_Rate['Unemployment-detrended'] - Unemployment_Rate[
+        'Unemployment-seasonal']
 
-corr_matrix = combined_quarterly.corr()
+    fig, ax_str = plt.subplots(4, figsize=(16, 9))
 
-fig, ax = plt.subplots(figsize=(8, 6))
+    Unemployment_Rate["2019":"2024"]['Unemployment Rate'].plot(label='Original', ax=ax_str[0])
+    Unemployment_Rate["2019":"2024"]['MA-4'].plot(color='orange', label='MA-24 Trend', ax=ax_str[1])
+    Unemployment_Rate["2019":"2024"]['Unemployment-seasonal'].plot(color='blue', label='Seasonal', ax=ax_str[2])
+    Unemployment_Rate["2019":"2024"]['Unemployment_residual'].plot(color='green', label='Residual', ax=ax_str[3])
 
-cax = ax.matshow(corr_matrix, cmap='coolwarm', vmin=-1, vmax=1)
+    fig.legend()
+    plt.tight_layout()
 
-fig.colorbar(cax)
+    st.pyplot(fig)
 
-ax.set_xticks(np.arange(len(corr_matrix.columns)))
-ax.set_yticks(np.arange(len(corr_matrix.columns)))
+    arima_params = arima_2_2_1.params
+    pvalues = arima_2_2_1.pvalues
+    aic = arima_2_2_1.aic
 
-ax.set_xticklabels(corr_matrix.columns, fontsize=6)
-ax.set_yticklabels(corr_matrix.columns, rotation=90, fontsize=6)
+    st.subheader('ARIMA Model Important Values')
+    st.markdown(f"**ARIMA(2, 2, 1) Coefficients:**")
+    for param, value in arima_params.items():
+        st.markdown(f"- **{param}:** {value:.4f}")
 
-for i in range(len(corr_matrix.columns)):
-    for j in range(len(corr_matrix.columns)):
-        ax.text(j, i, f'{corr_matrix.iloc[i, j]:.2f}', ha='center', va='center', color='black')
+    st.markdown(f"**P-values for the ARIMA model:**")
+    for param, p_value in pvalues.items():
+        st.markdown(f"- **{param}:** {p_value:.4f}")
 
-plt.tight_layout()
-st.pyplot(fig)
+    st.markdown(f"**AIC (Akaike Information Criterion):** {aic:.2f}")
